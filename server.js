97812485b0931ba7a37ea4f35b2c3b11cc1756f9
Valerian <valnimirenco@gmail.com>
2020-12-18 20:39:13 -0800

    solved merging conflicts 2.0

--- server.js
+++ server.js
@@ -8,11 +8,7 @@ var exphbs = require("express-handlebars");
 var passport = require("./config/passport");
 
 // Setting up port and requiring models for syncing
-<<<<<<< HEAD
 var PORT = process.env.PORT || 8080;
-=======
-var PORT = process.env.PORT || 8081;
->>>>>>> develop
 var db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
@@ -26,7 +22,9 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // We need to use sessions to keep track of our user's login status
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -38,9 +36,13 @@ require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
   });
 });
 
