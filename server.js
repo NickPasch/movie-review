1d04c5231b820639e6020a3aea356ed0311ca8f4
Nick Pasch <paschnick100@gmail.com>
2020-12-20 13:06:44 -0700

    making everything smooth

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 // Requiring necessary npm packages
-const express = require("express");
-const session = require("express-session");
+var express = require("express");
+var session = require("express-session");
 // Requiring passport as we've configured it
-const exphbs = require("express-handlebars");
-const passport = require("./config/passport");
+var exphbs = require("express-handlebars");
+var passport = require("./config/passport");
 
 // Setting up port and requiring models for syncing
-const PORT = process.env.PORT || 8080;
-const db = require("./models");
+var PORT = process.env.PORT || 8080;
+var db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
-const app = express();
+var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
